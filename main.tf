c7814ca56c229fa398d5989c5685efafd0d1cdb2
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-23 00:39:21 +0100

    added provider block ti the root model

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = ">= 2.7.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
 
 provider "aws" {
 
