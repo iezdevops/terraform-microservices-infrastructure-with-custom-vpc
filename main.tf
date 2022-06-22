65acc3cc0c9565c2eb0c2ac05314c1f2a2762836
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-23 00:42:54 +0100

    added provider org and workspace block ti the root model

--- main.tf
+++ main.tf
@@ -4,6 +4,14 @@ terraform {
       source  = "hashicorp/aws"
     }
   }
+
+  cloud {
+    organization = "Wearslot"
+
+    workspaces {
+      name = "infrastructure"
+    }
+  }
 }
 
 provider "aws" {
