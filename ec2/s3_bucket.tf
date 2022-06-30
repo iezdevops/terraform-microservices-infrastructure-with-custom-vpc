511e4577ad1e336a15ad5af54bcbb5c3ae8ecddd
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-30 01:51:34 +0100

    testing redeploy after completing VCS setup

--- ec2/s3_bucket.tf
+++ ec2/s3_bucket.tf
@@ -1,10 +1,10 @@
 
 resource "aws_s3_bucket" "s3_app_storage" {
 
-  bucket = "${var.project_name}-bucket"
+	bucket = "${var.project_name}-bucket"
 }
 
 resource "aws_s3_bucket_acl" "s3_app_acl" {
-  bucket = aws_s3_bucket.s3_app_storage.id
-  acl    = "private"
+	bucket = aws_s3_bucket.s3_app_storage.id
+	acl    = "private"
 }
