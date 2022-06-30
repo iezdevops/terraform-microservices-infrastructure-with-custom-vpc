f0cb3e41df3fac0bc3ee0ad47aba41143545775c
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-30 02:36:40 +0100

    remove image scanning on ecr_repo

--- ecs/ecr_repo.tf
+++ ecs/ecr_repo.tf
@@ -8,9 +8,9 @@ resource "aws_ecr_repository" "ecr_repo" {
   name = "${var.project_name}"
   image_tag_mutability = "MUTABLE"
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+  # image_scanning_configuration {
+  #   scan_on_push = true
+  # }
 
 }
 
