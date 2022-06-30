ac816d88a59c66afdfaa499d3826e117a3967d72
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-30 01:45:28 +0100

    added private acl for s3 bucket

--- ec2/s3_bucket.tf
+++ ec2/s3_bucket.tf
@@ -3,3 +3,8 @@ resource "aws_s3_bucket" "s3_app_storage" {
 
     bucket = "${var.project_name}-bucket"
 }
+
+resource "aws_s3_bucket_acl" "s3_app_acl" {
+  bucket = aws_s3_bucket.s3_app_storage.id
+  acl    = "private"
+}
