cf53782fb73892368f59182dff847c942ca5536e
donalkens <akinniyiakinpelumi@gmail.com>
2022-06-30 01:46:29 +0100

    pushing to master for deploment

--- ec2/s3_bucket.tf
+++ ec2/s3_bucket.tf
@@ -1,7 +1,7 @@
 
-resource "aws_s3_bucket" "s3_app_storage" { 
+resource "aws_s3_bucket" "s3_app_storage" {
 
-    bucket = "${var.project_name}-bucket"
+  bucket = "${var.project_name}-bucket"
 }
 
 resource "aws_s3_bucket_acl" "s3_app_acl" {
